39e15f41209eb0302eeae89234215f8acaf6b4bb
Saikumar Kakumanu <s.kakumanu@zensar.com>
2022-07-15 14:39:33 +0530

    Hands on

--- EFTraining/DataBaseFirst/DataBaseFirst/Program.cs
+++ EFTraining/DataBaseFirst/DataBaseFirst/Program.cs
@@ -8,9 +8,24 @@ namespace DataBaseFirst
 {
     class Program
     {
+        static ZensarDBEntities dbcontext = new ZensarDBEntities();
+        static tblEmployee emp = new tblEmployee();
+
+        public void DisplayEmp()
+        {
+            var deptlist = from d in dbcontext.tblEmployees
+                           select d;
+
+            foreach (var v in deptlist)
+            {
+                Console.WriteLine(v.Empid + "  " + v.EmpName + "  " + v.Gender + "  " + v.Salary + "  " + v.Email + "  " + v.DId + "  " + v.Phoneno);
+            }
+        }
         static void Main(string[] args)
         {
-
+           Program p = new Program();
+            p.DisplayEmp();
+            Console.ReadLine();
         }
     }
 }
